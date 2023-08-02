c49dac0e2e891d9b034d1f948b82970fc2c81ba0
PedroVic12 <pedrovictor.rveras12@gmail.com>
2023-08-02 15:13:30 -0300

    .

--- camorim_relatoios_os/android/app/src/main/kotlin/com/example/camorim_relatoios_os/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.camorim_relatoios_os
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- camorim_relatoios_os/lib/models/relatorio_model.dart
+++
@@ -1,9 +0,0 @@
-class RelatorioModel {
-  final String? nome_rebocador;
-  final String? descricao_falha;
-  final String? servico_executado;
-  final bool? servico_finalizado;
-
-  RelatorioModel(this.nome_rebocador, this.descricao_falha,
-      this.servico_executado, this.servico_finalizado);
-}
